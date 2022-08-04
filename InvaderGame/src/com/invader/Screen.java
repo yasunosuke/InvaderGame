bcf6e83c6d2b2228959d45c3554278f702cf9328
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2022-08-04 21:25:36 +0900

    feat: New class that has stuff for the screen

---
+++ InvaderGame/src/com/invader/Screen.java
@@ -0,0 +1,54 @@
+package com.invader;
+
+import java.util.List;
+
+public class Screen {
+	static final int SCREEN_WIDTH = 32;
+	static final int SCREEN_HEIGHT = 24;
+	static final int INVADER_COLUMN = 11;
+	static final int INVADER_ROW = 5;
+	
+	private List<Invader> invaders;
+	private String[][] field = new String[SCREEN_HEIGHT][SCREEN_WIDTH];
+	
+	public Screen(List<Invader> invaders) {
+		super();
+		this.invaders = invaders;
+	}
+
+	public void draw() {
+		
+		for(int y = 0; y < SCREEN_HEIGHT; y++) {
+			for(int x = 0; x < SCREEN_WIDTH; x++) {
+				field[y][x] = "E";
+			}
+		}
+		
+//		Invader‚ðfield‚É‘‚«‚±‚Ý
+//		1
+		for(Invader i: this.invaders) {
+			field[i.getPositionY()][i.getPositionX()] = i.getCharacter();
+		}
+//		2
+//		for(int i = 0; i < this.invaders.size(); i++) {
+//			Invader invader = this.invaders.get(i);
+//			String invaderCharacter = invader.getCharacter();
+//			field[invader.getPositionY()][invader.getPositionX()] = invaderCharacter;
+//		}
+//		3
+//		for(int y = 0; y < Screen.INVADER_ROW; y++) {
+//			for(int x = 0; x < Screen.INVADER_COLUMN; x++) {
+//				Invader invader = invaders.get(y * Screen.INVADER_COLUMN + x);
+//				String invaderCharacter = invader.getCharacter();
+//				field[invader.getPositionY()][invader.getPositionX()] = invaderCharacter;
+//			}
+//		}
+		
+		for(int y = 0; y < SCREEN_HEIGHT; y++) {
+			for(int x = 0; x < SCREEN_WIDTH; x++) {
+				System.out.print(field[y][x]);
+			}
+			System.out.println("");
+		}
+	}
+}
