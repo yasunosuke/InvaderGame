5cb3e911c4f6b5927feae481dd2321ad7d210307
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2022-08-22 12:47:17 +0900

    feat: wrote game's main loop

--- InvaderGame/src/com/invader/InvaderGame.java
+++ InvaderGame/src/com/invader/InvaderGame.java
@@ -4,10 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InvaderGame {
+	
+	private static final int FPS = 1;
+	private static final int INTERVAL = 1000/FPS;
 
 	public static void main(String[] args) {
 		Screen screen = new Screen(prepareInvaders());
-		screen.draw();
+		
+		long time1 = System.currentTimeMillis();
+//		メインループ
+		while(true) {
+			long time2 = System.currentTimeMillis();
+//			インターバル時間経過したらスクリーン描画
+			if(time2 - time1 >= INTERVAL) {
+				time1 = System.currentTimeMillis();
+				screen.draw();
+			}
+		}
 	}
 	
 	private static void Init() {
