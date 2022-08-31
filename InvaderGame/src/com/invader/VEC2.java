d3f869bd195b7cd8eb241cea93549104c398f77e
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2022-09-01 01:09:45 +0900

    feat: A new Class
    VEC2
    two-dimensional vector

---
+++ InvaderGame/src/com/invader/VEC2.java
@@ -0,0 +1,21 @@
+package com.invader;
+
+public class VEC2 {
+	private int x;
+	private int y;
+	
+	public VEC2(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+}
